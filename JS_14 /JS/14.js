63c9ba96e365232cd128672bc9e6424854b961bc
IngaPosypayko <gusgus@ukr.net>
2017-07-06 16:42:45 +0300

    ädd 14 hw

--- JS_14 /JS/14.js
+++ JS_14 /JS/14.js
@@ -1,39 +1,84 @@
 
-'use strict'
-var listOfQuestions = [
-    // {header: 'ТЕСТ ПО МАТЕМАТИКЕ'},
+'use strict';
+var listOfQuestions = {
+    header: 'ТЕСТ ПО МАТЕМАТИКЕ',
+    items: [
+        {
+            id:0,
+            title: '5+5=?',
+            answers: ['10', '12', '15'],
+            correctAnswer: 10
+        },
+        {
+            id:1,
+            title: '2+2=?',
+            answers: ['8', '4', '6', '10'],
+            correctAnswer: '4'
+        },
+        {
+            id:2,
+            title: '3+3=?',
+            answers: ['12', '6'],
+            correctAnswer: '6'
+        }
 
-      {
-        title: '5+5=?',
-        answers: ['10', '12', '15'],
-        correctAnswer: '10'
-      },
-      {
-        title: '2+2=?',
-        answers: ['8', '4', '6', '10'],
-          correctAnswer:'4'
-      },
-    {
-        title: '3+3=?',
-        answers: ['12', '6'],
-        correctAnswer: '6'
-    }
-
-     ];
-
-
+    ]
+};
 
 
 var str=JSON.stringify(listOfQuestions);
-console.log(str);
-
 var obj=JSON.parse(str);
-console.log(obj);
+
 
 var parent = document.getElementById('testWrapper');
-// console.log(parent);
 var source = document.getElementById('myTest').textContent;
-// console.log(source);
 var template = _.template(source);
-// console.log(template);
-parent.innerHTML = template(listOfQuestions);
+parent.innerHTML = template(obj);
+
+var elem = document.getElementsByClassName('answ');
+console.log(elem);
+
+//add event
+_.forEach(elem,function (item) {
+    item.addEventListener('click', click);
+    });
+
+
+function click(event) {
+    var userAnswer=event.target.parentElement.innerText;
+    console.log('user answer='+userAnswer);
+    var corrAnsw=listOfQuestions.items[0].correctAnswer;
+if (userAnswer==corrAnsw){ alert('SMART student!')} else{ alert('STUPID student!')};
+
+    var currentTask=event.target.parentNode.parentNode.innerHTML.substr(0,30);
+ console.log(currentTask);
+
+         _.forEach(listOfQuestions.items.title,function (item) {
+             console.log('item'+item);
+             if (item== currentTask){
+                 var taskId=listOfQuestions.items.id;
+                 alert(taskId+'вопрос');
+             }
+
+         })
+    var numberQuestion=listOfQuestions.items[0].answers.indexOf('10');
+    console.log('umber'+numberQuestion);
+
+
+
+}
+
+
+
+// var userAnswer=event.target.parentElement.innerText;
+// console.log('user answer='+userAnswer);
+//
+// if userAnswer=items.correctAnswer
+//
+//     var ev=event.target.parentNode.parentNode.innerText;
+// console.log(ev);
+
+
+var numberQuestion=listOfQuestions.items[0];
+console.log('umber'+numberQuestion);
+
