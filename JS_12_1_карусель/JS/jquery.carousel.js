62f49a435e1b734ffb861e0d577aa2d0643bbddd
IngaPosypayko <gusgus@ukr.net>
2017-07-11 13:57:21 +0300

    carousel

--- JS_12_1_карусель/JS/jquery.carousel.js
+++ JS_12_1_карусель/JS/jquery.carousel.js
@@ -1,28 +1,30 @@
 
 
-$(document).ready(function() {
+function carousel() {
     var leftUIEl = $('.carousel-arrow-left');
     var rightUIEl = $('.carousel-arrow-right');
     var elementsList = $('.carousel-list');
- 
+
     var pixelsOffset = 125;
     var currentLeftValue = 0;
     var elementsCount = elementsList.find('li').length;
-    var minimumOffset = - ((elementsCount - 5) * pixelsOffset);
+    var minimumOffset = -((elementsCount - 5) * pixelsOffset);
     var maximumOffset = 0;
- 
-    leftUIEl.click(function() {        
+
+    leftUIEl.click(function () {
         if (currentLeftValue != maximumOffset) {
             currentLeftValue += 125;
-            elementsList.animate({ left : currentLeftValue + "px"}, 500);
-        }        
+            elementsList.animate({left: currentLeftValue + "px"}, 500);
+        }
     });
- 
-    rightUIEl.click(function() {        
+
+    rightUIEl.click(function () {
         if (currentLeftValue != minimumOffset) {
             currentLeftValue -= 125;
-            elementsList.animate({ left : currentLeftValue + "px"}, 500);
-        }        
+            elementsList.animate({left: currentLeftValue + "px"}, 500);
+        }
     });
-});
+
+}
+
 
