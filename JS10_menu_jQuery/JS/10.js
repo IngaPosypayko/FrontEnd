7b9568c3cb8d987b578020dda544c81ef8a8c970
IngaPosypayko <gusgus@ukr.net>
2017-07-12 15:46:19 +0300

    10 hw added

---
+++ JS10_menu_jQuery/JS/10.js
@@ -0,0 +1,40 @@
+
+'use strict'
+$(document).ready(function () {
+
+    var $position;
+
+    $('.main_menu>li').hover(function () {
+
+        $(this).children('ul').stop (false, true).slideDown(400);
+
+    }, function () {
+
+        $(this).children('ul').stop (false, true).slideUp(1);
+
+
+    });
+
+    $('.sub_menu>li').hover(function () {
+
+        $position = $(this).position();
+
+        $(this).children('ul').css({
+
+            'left': 150,
+            'top': $position.top
+
+        }).stop (false, true).slideDown(400);
+    }, function () {
+
+        $(this).children('ul').stop (false, true).slideUp(1);
+
+    });
+
+
+    $('.sub_menu').prev().append(' >');
+
+});
+
+
+
--- JS10_select/JS/10.js
+++ JS10_select/JS/10.js
@@ -1,29 +1,6 @@
 
 'use strict'
 
-$(function() {
-    $('.jcarousel').jcarousel({
-        // Configuration goes here
-    });
 
-
-$('.jcarousel-prev').jcarouselControl({
-        target: '-=1'
-    });
-
-    $('.jcarousel-next').jcarouselControl({
-        target: '+=1'
-    });
-
-
-
- $('.jcarousel-pagination').jcarouselPagination({
-        item: function(page) {
-            return '<a href="#' + page + '">' + page + '</a>';
-        }
-    });
-
-
-});
 
 
